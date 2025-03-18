d33dd244ed0af38ee6c6f92ca94e0d6124aea933
Vincent Mokua <mokua.vinny@gmail.com>
2025-03-18 11:45:29 -0700

    init

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,7 +6,7 @@ from api.v1.views import app_views
 from flask import Flask, Blueprint, jsonify, make_response
 from flask_cors import CORS
 
-
+#This is the first part
 app = Flask(__name__)
 app.register_blueprint(app_views)
 cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
