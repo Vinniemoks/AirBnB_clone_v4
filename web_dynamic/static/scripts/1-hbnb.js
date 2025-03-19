5adc5d3a6c3b1f08948bfdae1b10d248d4284162
Vincent Mokua <mokua.vinny@gmail.com>
2025-03-19 00:09:47 -0700

    changed

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,17 @@
+/* global $ */
+$(document).ready(function () {
+  const amenities = {};
+
+  $('input[type="checkbox"]').change(function () {
+    const amenityId = $(this).data('id');
+    const amenityName = $(this).data('name');
+
+    if ($(this).is(':checked')) {
+      amenities[amenityId] = amenityName;
+    } else {
+      delete amenities[amenityId];
+    }
+
+    $('.amenities h4').text(Object.values(amenities).join(', '));
+  });
+});
---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,26 @@
+/* global $ */
+$(document).ready(function () {
+  const amenities = {};
+
+  $('input[type="checkbox"]').change(function () {
+    const amenityId = $(this).data('id');
+    const amenityName = $(this).data('name');
+
+    if ($(this).is(':checked')) {
+      amenities[amenityId] = amenityName;
+    } else {
+      delete amenities[amenityId];
+    }
+
+    $('.amenities h4').text(Object.values(amenities).join(', '));
+  });
+
+  // Check API status
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (response) {
+    if (response.status === 'OK') {
+      $('div#api_status').addClass('available');
+    } else {
+      $('div#api_status').removeClass('available');
+    }
+  });
+});
